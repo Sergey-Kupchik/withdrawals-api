d7c6184e6bc148c558816126106165c81d09cbe5
“Sergey-Kupchik” <kupchikrabota@gmail.com>
2023-04-07 13:52:12 -0400

    CI13 updated time

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getWithdrawals(): string {
-    return '1.42 4/7/23 withdrawals-api running... ';
+    return '1.51 4/7/23 withdrawals-api running... ';
   }
 }
